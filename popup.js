69b21879fec6b7a4860cf629e75d971eb5f0d7c1
Protobrine <gargarsenprotobrine@gmail.com>
2023-02-13 22:47:56 +0800

    Start and pause somewhat updated

--- popup.js
+++ popup.js
@@ -5,23 +5,60 @@ const divblock = document.getElementById("divblock");
 const divtiming = document.getElementById("divtiming");
 const divother = document.getElementById("divother");
 const startBtn = document.getElementById('start');
+const pauseBtn = document.getElementById('pause');
+let convertTotal;
 
 btn_blocking.addEventListener('click', () =>{
     divblock.style.display='block';
     divtiming.style.display='none';
     divother.style.display='none';
 });
+
 btn_timing.addEventListener('click', () =>{
     divblock.style.display='none';
     divtiming.style.display='block';
     divother.style.display='none';
 });
+
 btn_otherThings.addEventListener('click', () =>{
     divblock.style.display='none';
     divtiming.style.display='none';
     divother.style.display='block';
 });
+
 startBtn.addEventListener('click' , () =>{
     const userHours = document.getElementById('hours').value;
-    alert('you gae ' + userHours);
-})
+    const userMinutes = document.getElementById('minutes').value;
+    const userSeconds = document.getElementById('seconds').value;
+    let totalSeconds;
+    
+    totalSeconds = +userSeconds + +(userMinutes*60) + +(userHours*3600);
+    document.getElementById("start").disabled = true;
+    document.getElementById("pause").disabled = false;
+    yes = setInterval(countdownUpdate, 1000);
+    function countdownUpdate() {
+        const updateHours = Math.floor(totalSeconds / 3600).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false});
+        const updateMinutes = Math.floor((totalSeconds % 3600)/60).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false});
+        const updateSeconds = (totalSeconds % 60).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false});
+        
+        hrs.innerHTML = updateHours;
+        min.innerHTML = updateMinutes;
+        sec.innerHTML = updateSeconds;
+        if (totalSeconds <= 0) {
+            clearInterval(yes);
+            document.getElementById('hours').innerHTML = '00';
+            document.getElementById('minutes').innerHTML = '00';
+            document.getElementById('seconds').innerHTML = '00';
+            document.getElementById("start").disabled = false;
+        }
+        totalSeconds--;
+    }
+});
+
+pauseBtn.addEventListener('click' , () => {
+    clearInterval(yes);
+    document.getElementById("start").disabled = false;
+    document.getElementById("pause").disabled = true;
+});
+
+
